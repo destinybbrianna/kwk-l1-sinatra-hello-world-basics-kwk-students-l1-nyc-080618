a6c5b46743dd16f5e0e5255dd24f5874bb676ed9
destinybbrianna <destinybbrianna@gmail.com>
2018-08-15 17:37:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
+# Define your GET '/' route below and respond with "Hello, World!"
+
+
 class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do
+ "Hello World!"
+ end
 end
